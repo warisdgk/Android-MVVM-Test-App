11537cb8d24221a6eec0a6d037f4643cfb144f42
Muhammad Waris <warisdgk@gmail.com>
2020-11-29 00:57:49 +0500

    feat: BaseDataSource class added

---
+++ app/src/main/java/com/warisdgk/data/remote/BaseDataSource.kt
@@ -0,0 +1,25 @@
+package com.warisdgk.data.remote
+
+import com.warisdgk.utilities.Resource
+import retrofit2.Response
+
+abstract class BaseDataSource {
+
+    protected suspend fun <T> getResult(call: suspend () -> Response<T>): Resource<T> {
+        try {
+            val response = call()
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body != null) return Resource.success(body)
+            }
+            return error(" ${response.code()} ${response.message()}")
+        } catch (e: Exception) {
+            return error(e.message ?: e.toString())
+        }
+    }
+
+    private fun <T> error(message: String): Resource<T> {
+        return Resource.error("Network call failed with message: $message")
+    }
+
+}
