9643233cf2c5e532c2b05cdcd762284cc704df2f
Muhammad Waris <warisdgk@gmail.com>
2020-11-29 01:01:20 +0500

    feat: Util class for data access strategy added

---
+++ app/src/main/java/com/warisdgk/utilities/DataAccessStrategy.kt
@@ -0,0 +1,23 @@
+package com.warisdgk.utilities
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
+import kotlinx.coroutines.Dispatchers
+
+fun <T, A> performGetOperation(
+    databaseQuery: () -> LiveData<T>,
+    networkCall: suspend () -> Resource<A>,
+    saveCallResult: suspend (A) -> Unit
+): LiveData<Resource<T>> =
+    liveData(Dispatchers.IO) {
+        emit(Resource.loading())
+        val responseStatus = networkCall.invoke()
+        if (responseStatus.status == Resource.Status.SUCCESS) {
+            saveCallResult(responseStatus.data!!)
+            val source = databaseQuery.invoke().map { Resource.success(it) }
+            emitSource(source)
+        } else if (responseStatus.status == Resource.Status.ERROR) {
+            emit(Resource.error(responseStatus.message!!))
+        }
+    }
