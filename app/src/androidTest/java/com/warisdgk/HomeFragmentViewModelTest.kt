5be4710007ef9ada8ab3ec8e2233673de3e04359
Muhammad Waris <warisdgk@gmail.com>
2020-11-29 15:54:50 +0500

    test: HomeFragmentViewModelTest.kt class added to test default values

---
+++ app/src/androidTest/java/com/warisdgk/HomeFragmentViewModelTest.kt
@@ -0,0 +1,52 @@
+package com.warisdgk
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+import com.warisdgk.data.local.AppDatabase
+import com.warisdgk.data.repository.PostsRepository
+import com.warisdgk.ui.home.HomeViewModel
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.*
+import org.junit.rules.RuleChain
+import javax.inject.Inject
+import kotlin.jvm.Throws
+
+@HiltAndroidTest
+class HomeFragmentViewModelTest {
+
+    private lateinit var appDatabase: AppDatabase
+    private lateinit var viewModel: HomeViewModel
+    private val hiltRule = HiltAndroidRule(this)
+    private val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val rule = RuleChain
+        .outerRule(hiltRule)
+        .around(instantTaskExecutorRule)!!
+
+    @Inject
+    lateinit var plantRepository: PostsRepository
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        appDatabase = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+
+        viewModel = HomeViewModel(plantRepository)
+    }
+
+    @After
+    fun closeDB() {
+        appDatabase.close()
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun testDefaultValue() {
+        Assert.assertFalse(viewModel.allowNavigate)
+    }
+}
